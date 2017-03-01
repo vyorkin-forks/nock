8cf946af4314839f433b408c345244443c0390e7
Ian Walker-Sperber <ianwsperber@gmail.com>
2017-03-01 10:20:37 -0800

    #841 Reproduce timeout in got & popsicle

--- tests/versioned/common.js
+++ tests/versioned/common.js
@@ -22,25 +22,53 @@ const nock = require('../../');
  * @param {string} name Lib we're testing
  */
 const runCommonTests = (makeRequest, name) => {
-  const test = (description, cb) => {
-    return tap.test(`${name}: ${description}`, cb);
+  const throwSuccess = (res) => {
+    const err = new Error(`Unexpected resolution (${res.statusCode})`);
+    err.unexpected = true;
+
+    throw err;
   };
 
-  test('basic intercept', (t) => {
-    nock('http://www.example.com')
-      .get('/')
-      .reply(200, 'OK');
+  tap.test(`Common tests: ${name}`, (t) => {
+    t.plan(2);
 
-    return makeRequest({
-        uri: 'http://www.example.com'
-      })
-      .then((res) => {
-        t.equal(res.statusCode, 200);
-        t.deepEqual(res.body, 'OK');
+    t.beforeEach((done) => {
+      nock.cleanAll();
+      done();
+    });
 
-        t.end();
-      })
-      .catch(t.threw);
+    t.test('basic intercept', (t) => {
+      nock('http://www.example.com')
+        .get('/')
+        .reply(200, 'OK');
+
+      return makeRequest({
+          uri: 'http://www.example.com'
+        })
+        .then((res) => {
+          t.equal(res.statusCode, 200);
+          t.deepEqual(res.body, 'OK');
+
+          t.end();
+        })
+        .catch(t.threw);
+    });
+
+    t.test('incorrect path', (t) => {
+      nock('http://www.example.com')
+        .get('/foo')
+        .reply(200, 'OK');
+
+      return makeRequest({
+          uri: 'http://www.example.com/bar'
+        })
+        .then(throwSuccess)
+        .catch((err) => {
+          t.match(err.message, /No match for request/);
+          t.end();
+        })
+        .catch(t.threw);
+    });
   });
 };
 
